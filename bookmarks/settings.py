0e926c68dfb406d3ff190e7927b6c03bebe37907
Xens Chan <xentisize@gmail.com>
2019-03-18 01:35:58 +0800

    Working on Profile

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -121,3 +121,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
