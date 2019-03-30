0beeb71ec37b73f29980065bbe9eb3e78edef31f
Xens Chan <xentisize@gmail.com>
2019-03-30 23:50:23 +0800

    Finished followers

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -132,3 +132,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'account.authentication.EmailAuthBackend',
 ]
+
+from django.urls import reverse_lazy
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
